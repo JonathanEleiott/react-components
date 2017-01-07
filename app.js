a6a3d44940dd74980ad19ab8dc03941d898b4fa6
JonathanEleiott <jonathan.eleiott@gmail.com>
2017-01-06 17:04:37 -0800

    Complete all tasks for react components

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
-// TODO
+// Create a variable named GroceryList
+  // Create a div
+  // create a list
+    // Put apple, tomatoes and basil in the list
+
+let GroceryList = () => (
+  <div>
+    <h2>Grocery List</ h2>
+      <List listOfGroceries={['Appples', 'Basil']}/>
+  </div>
+)
+
+class ListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false
+    }
+  } 
+
+  eventActivated() {
+    this.setState({
+      done: !this.state.done
+    })
+  }
+
+  render() {
+    var style = {
+      'fontWeight': this.state.done ? 'bold' : 'normal'
+    }
+    return (
+      <li style={style} onMouseEnter={this.eventActivated.bind(this)} onMouseLeave={this.eventActivated.bind(this)}>{this.props.item}</li>
+    );    
+  } 
+}
+
+let List = (props) =>
+  (<ul>
+    {props.listOfGroceries.map(item => 
+      <ListItem item={item} />     
+    )}
+  </ul>)
+
+
+
+ReactDOM.render(<GroceryList />, document.getElementById('app'));
